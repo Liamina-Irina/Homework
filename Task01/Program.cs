66459b640bfd79ccffb6d6b0e7a2374c65709d38
Irina <liamina2207@gmail.com>
2022-12-15 21:48:32 +0300

    Added changes

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -12,8 +12,8 @@ Console.WriteLine("Введите первое число");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int numberB = Convert.ToInt32(Console.ReadLine());
-int square= numberB * numberB;
-if(square == numberA)
+int square = numberB * numberB;
+if (square == numberA)
 {
     Console.WriteLine("Первое число является квадратом второго");
 }
